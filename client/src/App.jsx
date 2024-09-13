a9fee0779428aed67aca05bd4b821d8e5b441d14
patrickmar <rdi@avante-cs.com>
2024-09-13 10:36:23 +0100

    egain commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,37 @@
-import styles from './style';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import About from './pages/About';
-import Products from './pages/Products';
-import News from './pages/News';
-import Contacts from './pages/Contacts';
-import Consulting from './pages/services/Consulting';
-import Software from './pages/services/Software';
-import EgainCollaboration from './pages/services/EgainCollaboration';
-import EgainKnowledge from './pages/services/EgainKnowledge';
-import SocialMediaMgt from './pages/services/SocialMediaMgt';
-import Sas from './pages/services/Sas';
-import OracleDatabase from './pages/services/OracleDatabase';
-import WebDev from './pages/services/WebDev';
-import Agencies from './pages/clients/Agencies';
-import Financial from './pages/clients/Financial';
-import Telecoms from './pages/clients/Telecoms';
-import SME from './pages/clients/SME';
-import Goods from './pages/clients/Goods';
-import NewsExplicit from './components/NewsExplicit';
-import { Toaster } from 'react-hot-toast';
-import ScrollToTop from './components/ScrolToTop';
-import Implementation from './pages/services/consulting/Implementation';
-import ProjectManagement from './pages/services/consulting/ProjectManagement';
-import UserResearch from './pages/services/consulting/UserResearch';
-import MarketSurvey from './pages/services/consulting/MarketSurvey';
-import FeasibilityStudies from './pages/services/consulting/FeasibilityStudies';
-import Register from './pages/auth/Register';
+import styles from "./style";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import News from "./pages/News";
+import Contacts from "./pages/Contacts";
+import Consulting from "./pages/services/Consulting";
+import Software from "./pages/services/Software";
+import EgainCollaboration from "./pages/services/EgainCollaboration";
+import EgainKnowledge from "./pages/services/EgainKnowledge";
+import SocialMediaMgt from "./pages/services/SocialMediaMgt";
+import Sas from "./pages/services/Sas";
+import OracleDatabase from "./pages/services/OracleDatabase";
+import WebDev from "./pages/services/WebDev";
+import Agencies from "./pages/clients/Agencies";
+import Financial from "./pages/clients/Financial";
+import Telecoms from "./pages/clients/Telecoms";
+import SME from "./pages/clients/SME";
+import Goods from "./pages/clients/Goods";
+import NewsExplicit from "./components/NewsExplicit";
+import { Toaster } from "react-hot-toast";
+import ScrollToTop from "./components/ScrolToTop";
+import Implementation from "./pages/services/consulting/Implementation";
+import ProjectManagement from "./pages/services/consulting/ProjectManagement";
+import UserResearch from "./pages/services/consulting/UserResearch";
+import MarketSurvey from "./pages/services/consulting/MarketSurvey";
+import FeasibilityStudies from "./pages/services/consulting/FeasibilityStudies";
+import Register from "./pages/auth/Register";
+import ActivityList from "./egain/ActivityList";
+import ActivitityResult from "./egain/ActivitityResult";
+import Login from "./egain/Login";
 
 const App = () => {
   return (
@@ -75,6 +78,9 @@ const App = () => {
           <Route path="/services/media" element={<SocialMediaMgt />} />
           <Route path="/services/oracle" element={<OracleDatabase />} />
           <Route path="/services/web" element={<WebDev />} />
+          <Route path="/egain/activity" element={<ActivityList />} />
+          <Route path="/egain/activity-result" element={<ActivitityResult />} />
+          <Route path="/egain/login" element={<Login />} />
         </Routes>
         <Toaster />
         <Footer />
